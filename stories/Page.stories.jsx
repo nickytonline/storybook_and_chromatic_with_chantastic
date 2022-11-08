c24b4a0a7b9d48a84b13a83ad24970c0f9388114
Nick Taylor <nick@iamdeveloper.com>
2022-11-08 10:26:59 -0500

    chore: added files from stream with Chan

--- stories/Page.stories.jsx
+++ stories/Page.stories.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { within, userEvent } from '@storybook/testing-library';
+import React from "react";
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
-import { Page } from './Page';
+import { Page } from "./Page";
 
 export default {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
@@ -20,6 +21,17 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
   await userEvent.click(loginButton);
+  expect(await canvas.getByRole("button", { name: /Log out/i }));
+};
+
+export const LoggedInAndOut = Template.bind({});
+LoggedInAndOut.play = async ({ canvasElement }) => {
+  await LoggedIn.play({ canvasElement });
+
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByRole("button", { name: /Log out/i });
+  await userEvent.click(loginButton);
+  expect(await canvas.getByRole("button", { name: /Log in/i }));
 };
